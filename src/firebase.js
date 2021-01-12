458b89084c2f52cf42e67f497c06e3be6bc838eb
Preyansh Kachhia <36520183+PreyanshK@users.noreply.github.com>
2021-01-12 16:12:06 -0500

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnOk7JZavM0yY-4BTuoTM5QtfI1qwD2QQ",
-  authDomain: "whatsapp-clone-9b278.firebaseapp.com",
-  projectId: "whatsapp-clone-9b278",
-  storageBucket: "whatsapp-clone-9b278.appspot.com",
-  messagingSenderId: "418968485886",
-  appId: "1:418968485886:web:a5f7b33bbc01bfa0cb4aaf",
-  measurementId: "G-6QSG0Q8C17"
+  // your firebase credentials go here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
